02b182b315ff1328facf0b97d64848d2d1f84926
Scott Wakefield <scott.wakefield@gmail.com>
2013-05-03 12:05:17 +0800

    Updating top nav bar to use bootstrap

--- interface/main/main_title.php
+++ interface/main/main_title.php
@@ -1,3 +1,4 @@
+<!DOCTYPE HTML>
 <?php
 /**
  * main_title.php - The main titlebar, at the top of the 'concurrent' layout.
@@ -7,8 +8,13 @@ include_once('../globals.php');
 ?>
 <html>
 <head>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1">
+		<!--<link rel=stylesheet href="<?php echo $css_header;?>" type="text/css">-->
+		<link rel=stylesheet href="../../library/css/bootstrap.css" type="text/css">
+		<script language='JavaScript' src="../../library/js/jquery-1.9.1.min.js"></script>
+		<script language='JavaScript' src="../../library/js/bootstrap.js"></script>
 
-<link rel="stylesheet" href="<?php echo $css_header;?>" type="text/css">
 <style type="text/css">
       .hidden {
         display:none;
@@ -68,67 +74,58 @@ function showhideMenu() {
 </script>
 </head>
 <body class="body_title">
-<?php
-$res = sqlQuery("select * from users where username='".$_SESSION{"authUser"}."'");
-?>
+	<?php
+		$res = sqlQuery("select * from users where username='".$_SESSION{"authUser"}."'");
+	?>
 
-<table cellspacing="0" cellpadding="0" width="100%" height="100%">
-<tr>
-<td align="left">
-<?php if ($GLOBALS['concurrent_layout']) { ?>
-	<table cellspacing="0" cellpadding="1" style="margin:0px 0px 0px 3px;">
-
-<?php if (acl_check('patients','demo','',array('write','addonly') )) { ?>
-<tr><td style="vertical-align:text-bottom;">
-		<a href='' class="css_button_small" style="margin:0px;vertical-align:top;" id='new0' onClick=" return top.window.parent.left_nav.loadFrame2('new0','RTop','new/new.php')">
-		<span><?php echo htmlspecialchars( xl('NEW PATIENT'), ENT_QUOTES); ?></span></a>
-    </td>
-    <td style="vertical-align:text-bottom;">
-            <a href='' class="css_button_small" style="margin:0px;vertical-align:top;display:none;" id='clear_active' onClick="javascript:parent.left_nav.clearactive();return false;">
-            <span><?php echo htmlspecialchars( xl('CLEAR ACTIVE PATIENT'), ENT_QUOTES); ?></span></a>
-    </td>
-</tr>
-<?php } //end of acl_check('patients','demo','',array('write','addonly') if ?>
-
-	<tr><td valign="baseline"><B>
-		<a class="text" style='vertical-align:text-bottom;' href="main_title.php" id='showMenuLink' onclick='javascript:showhideMenu();return false;'><?php xl('Hide Menu','e'); ?></a></B>
-	</td></tr></table>
-<?php } else { ?>
-&nbsp;
-<?php } ?>
-</td>
-<td style="margin:3px 0px 3px 0px;vertical-align:middle;">
-        <div style='margin-left:10px; float:left; display:none' id="current_patient_block">
-            <span class='text'><?php xl('Patient','e'); ?>:&nbsp;</span><span class='title_bar_top' id="current_patient"><b><?php xl('None','e'); ?></b></span>
-        </div>
-</td>
-<td style="margin:3px 0px 3px 0px;vertical-align:middle;" align="left">
-	<table cellspacing="0" cellpadding="1" ><tr><td>
-		<div style='margin-left:5px; float:left; display:none' id="past_encounter_block">
-			<span class='title_bar_top' id="past_encounter"><b><?php echo htmlspecialchars( xl('None'), ENT_QUOTES) ?></b></span>
-		</div></td></tr>
-	<tr><td valign="baseline" align="center">	
-        <div style='display:none' class='text' id="current_encounter_block" >
-            <span class='text'><?php xl('Selected Encounter','e'); ?>:&nbsp;</span><span class='title_bar_top' id="current_encounter"><b><?php xl('None','e'); ?></b></span>
-        </div></td></tr></table>
-</td>
-
-<td align="right">
-	<table cellspacing="0" cellpadding="1" style="margin:0px 3px 0px 0px;"><tr>
-		<td align="right" class="text" style="vertical-align:text-bottom;"><a href='main_title.php' onclick="javascript:parent.left_nav.goHome();return false;" ><?php xl('Home','e'); ?></a>
-		&nbsp;|&nbsp;
-		<a href="http://open-emr.org/wiki/index.php/OpenEMR_4.1.1_Users_Guide" target="_blank" id="help_link" >
-			<?php xl('Manual','e'); ?></a>&nbsp;</td>
-		<td align="right" style="vertical-align:top;"><a href="../logout.php?auth=logout" target="_top" class="css_button_small" style='float:right;' id="logout_link" onclick="top.restoreSession()" >
-			<span><?php echo htmlspecialchars( xl('Logout'), ENT_QUOTES) ?></span></a></td>
-	</tr><tr>
-		<td colspan='2' valign="baseline" align='right'><B>
-			<span class="text title_bar_top" title="<?php echo htmlspecialchars( xl('Authorization group') .': '.$_SESSION['authGroup'], ENT_QUOTES); ?>"><?php echo htmlspecialchars($res{"fname"}.' '.$res{"lname"},ENT_NOQUOTES); ?></span></span></td>
-    	</tr></table>
-</td>
-</tr>
-</table>
-
+	<div class="main_nav">
+		<div class="title_left">
+			<?php if ($GLOBALS['concurrent_layout']) { ?>
+				<div class="menu_hide" style="float:left; margin: 5px 10px 5px;" >	
+					<a class="btn" href='main_title.php' id='showMenuLink' onclick='javascript:showhideMenu();return false;'><?php xl('Hide Menu','e'); ?></a>
+					<?php if (acl_check('patients','demo','',array('write','addonly') )) { ?>
+					<a class="btn" href='' id='new0' onClick=" return top.window.parent.left_nav.loadFrame2('new0','RTop','new/new.php')"><?php xl('New Patient','e'); ?></a>
+					<a class="btn" href='' style="margin:0px;vertical-align:top;display:none;" id='clear_active' onClick="javascript:parent.left_nav.clearactive();return false;"><?php xl('Clear Active Patient','e'); ?></a>
+					<?php } ?>
+				</div>
+			<?php }  ?>
+		</div>
+		
+		<div class="title_middle">
+		
+			<div style='margin: 10px; float:left; display:none' id="current_patient_block">
+				<span class='text'><b><?php xl('Patient: ','e'); ?></b></span><span class='title_bar_top' id="current_patient"><b><?php xl('None','e'); ?></b></span>
+			</div>
+			
+			<div style='margin: 5px 10px 5px; float:left; display:none' id="past_encounter_block">
+				<span class='title_bar_top' id="past_encounter"><b><?php xl('None','e'); ?></b></span>
+			</div>
+			
+		
+			<div style='display:none' class='text' id="current_encounter_block" >
+				<span class='text'><?php xl('Selected Encounter','e'); ?></span><span class='title_bar_top' id="current_encounter"><b><?php xl('None','e'); ?></b></span>
+			</div>
+	
+		</div>
+	
+		<div class="title_right" style="float:right;">
+		
+			<div style="float:Left; margin: 10px;">
+				<span class="text title_bar_top" title="<?php echo htmlspecialchars( xl('Authorization group') .': '.$_SESSION['authGroup'], ENT_QUOTES); ?>"><b><?php echo xl('Logged in as:','e'); ?></b><?php echo htmlspecialchars($res{"fname"}.' '.$res{"lname"},ENT_NOQUOTES); ?></span>
+			</div>
+			
+			
+			<div style="float:right;" class="btn-group">
+				<div class="btn-group" style="margin: 5px 10px 5px;">
+				  <a class="btn" href='main_title.php' onclick="javascript:parent.left_nav.goHome();return false;" ><?php xl('Home','e'); ?></a>
+				  <a class="btn" href='http://open-emr.org/wiki/index.php/OpenEMR_4.1.1_Users_Guide' target="_blank" id="help_link" ><?php xl('Manual','e'); ?></a>
+				  <a class="btn" href='../logout.php?auth=logout' target="_top" id="logout_link" onclick="top.restoreSession()" ><?php xl('Logout','e'); ?></a>
+				</div>
+			</div>
+		</div>
+	</div>	
+	
+	
 <script type="text/javascript" language="javascript">
 parent.loadedFrameCount += 1;
 </script>
