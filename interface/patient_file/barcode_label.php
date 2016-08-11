aacd3ceee2a1136f1473665687ed097af7bc36d0
wakie87 <scott@npclinics.com.au>
2016-08-11 08:19:59 +0800

    small playaround

--- interface/patient_file/barcode_label.php
+++ interface/patient_file/barcode_label.php
@@ -88,7 +88,7 @@ $xres     = 1;    // width of the smallest bar in user units
 
 // ALLOCATE TCPDF RESSOURCE
 
-$pdf = new mPDF('', 'A4', '', '', 15, 15, 16, 16, 9, 9, 'L'); // set the orentation, unit of measure and size of the page
+$pdf = new mPDF('utf-8', 'A4', '', '', 15, 15, 16, 16, 9, 9, 'L'); // set the orentation, unit of measure and size of the page
 $pdf->AddPage(); 
 
 // BARCODE
@@ -113,10 +113,57 @@ $pdf->AddPage();
 
 //$x = $pdf->GetX();
 
+$linear = array(
+    'C128A'      => array('0123456789', 'CODE 128 A'),
+    'C128B'      => array('0123456789', 'CODE 128 B'),
+    'C128C'      => array('0123456789', 'CODE 128 C'),
+    'C128'       => array('0123456789', 'CODE 128'),
+    'C39E+'      => array('0123456789', 'CODE 39 EXTENDED + CHECKSUM'),
+    'C39E'       => array('0123456789', 'CODE 39 EXTENDED'),
+    'C39+'       => array('0123456789', 'CODE 39 + CHECKSUM'),
+    'C39'        => array('0123456789', 'CODE 39 - ANSI MH10.8M-1983 - USD-3 - 3 of 9'),
+    'C93'        => array('0123456789', 'CODE 93 - USS-93'),
+    'CODABAR'    => array('0123456789', 'CODABAR'),
+    'CODE11'     => array('0123456789', 'CODE 11'),
+    'EAN13'      => array('0123456789', 'EAN 13'),
+    'EAN2'       => array('12',         'EAN 2-Digits UPC-Based Extension'),
+    'EAN5'       => array('12345',      'EAN 5-Digits UPC-Based Extension'),
+    'EAN8'       => array('1234567',    'EAN 8'),
+    'I25+'       => array('0123456789', 'Interleaved 2 of 5 + CHECKSUM'),
+    'I25'        => array('0123456789', 'Interleaved 2 of 5'),
+    'IMB'        => array('0123456789', 'IMB - Intelligent Mail Barcode - Onecode - USPS-B-3200'),
+    'IMBPRE'     => array('fatdfatdfatdfatdfatdfatdfatdfatdfatdfatdfatdfatdfatdfatdfatdfatdf', 'IMB pre-processed'),
+    'KIX'        => array('0123456789', 'KIX (Klant index - Customer index)'),
+    'MSI+'       => array('0123456789', 'MSI + CHECKSUM (modulo 11)'),
+    'MSI'        => array('0123456789', 'MSI (Variation of Plessey code)'),
+    'PHARMA2T'   => array('0123456789', 'PHARMACODE TWO-TRACKS'),
+    'PHARMA'     => array('0123456789', 'PHARMACODE'),
+    'PLANET'     => array('0123456789', 'PLANET'),
+    'POSTNET'    => array('0123456789', 'POSTNET'),
+    'RMS4CC'     => array('0123456789', 'RMS4CC (Royal Mail 4-state Customer Bar Code)'),
+    'S25+'       => array('0123456789', 'Standard 2 of 5 + CHECKSUM'),
+    'S25'        => array('0123456789', 'Standard 2 of 5'),
+    'UPCA'       => array('0123456789', 'UPC-A'),
+    'UPCE'       => array('0123456789', 'UPC-E'),
+);
+
 $barcode = new \Com\Tecnick\Barcode\Barcode();
-$bobj = $barcode->getBarcodeObj('C128', '0123456789', '', '', 'black', array(0, 0, 0, 0));
-$bc = $bobj->getHtmlDiv();
-$pdf->WriteHTML($bc);
+//$examples = '<h3>Linear</h3>'."\n";   
+//foreach ($linear as $type => $code) {
+    $bobj = $barcode->getBarcodeObj($type, array('123456'), -3, -30, 'black', array(0, 0, 0, 0));
+    $examples = '<h4>'.$type.'</h4><p style="font-family:monospace;">'.$bobj->getHtmlDiv().'</p>'."\n";
+//}
+
+//echo "<h3>HTML DIV</h3><p style=\"font-family:monospace;\">".$bobj->getHtmlDiv()."</p>";
+//echo print_r($bobj);
+//echo $examples;
+//echo $bobj->getHtmlDiv();
+$pdf->WriteHTML('<h4>'.$type.'</h4><p style="font-family:monospace;">'.$bobj->getHtmlDiv().'</p>');
+
+// $bobj = $barcode->getBarcodeObj('C128', '0123456789', '', '', 'blue', array(0, 0, 0, 0));
+// $bc = $bobj->getHtmlDiv();
+// $pdf->WriteHTML($bc);
+
 //$pdf->write1DBarcode($code, $type, '', '', $w, $h, 1, $style, 'N');
 //$pdf->SetXY($x, (($h/2) + $fontSize + $z));
 //$pdf->Cell(0, 0, $code, 0, 0, 'C', FALSE, '', 0, FALSE, 'C', 'B');
