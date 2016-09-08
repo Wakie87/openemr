99538d3ccef9b67d52f8c1d34a5f2095130271b9
wakie87 <scott@npclinics.com.au>
2016-09-08 14:54:16 +0800

    Update index.php to use symfony

--- index.php
+++ index.php
@@ -1,10 +1,31 @@
 <?php
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
-// Set the site ID if required.  This must be done before any database
-// access is attempted.
+/**
+ *
+ * Copyright (C) 2008-2016 Scott Wakefield <scott@npclinics.com.au>
+ *
+ * LICENSE: This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://opensource.org/licenses/gpl-license.php>;.
+ *
+ * @package OpenEMR
+ * @author  Scott Wakefield <scott@npclinics.com.au>
+ * @link    http://www.open-emr.org
+ */
+
+require_once __DIR__.'/vendor/autoload.php';
+
+use OpenEMR\Framework\Http\Request;
+use OpenEMR\Framework\Http\Response;
+
+$request = Request::createFromGlobals();
+$response = new Response();
 
 if (!empty($_GET['site']))
     $site_id = $_GET['site'];
@@ -16,10 +37,14 @@ else
 if (empty($site_id) || preg_match('/[^A-Za-z0-9\\-.]/', $site_id))
     die("Site ID '".htmlspecialchars($site_id,ENT_NOQUOTES)."' contains invalid characters.");
 
-require_once "sites/$site_id/sqlconf.php";
+require_once __DIR__.'/sites/'.$site_id.'/sqlconf.php';
 
 if ($config == 1) {
-    header("Location: interface/login/login.php?site=$site_id");
+    $response->setStatusCode(302);
+    $response->headers->set('Location', 'interface/login/login.php?site='.$site_id);
 } else {
-    header("Location: setup.php?site=$site_id");
+    $response->setStatusCode(302);
+    $response->headers->set('Location', 'setup.php?site='.$site_id);
 }
+
+$response->send();
